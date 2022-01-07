8c1aea45fec07119eabf3027e4929ba472a709d9
Wesley Mays <60365549+WMays287@users.noreply.github.com>
2022-01-07 23:41:41 +0000

    Refactored some code, removing an especially ugly stream-XOR function

--- viewmem.py
+++ viewmem.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
 
-from PIL import Image, ImageColor
+from PIL import Image
 import argparse
-import sys
 
+# Get arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('rom', nargs=1, type=argparse.FileType('rb'))
 parser.add_argument('--offset', '-l', type=int, default=4)
 parser.add_argument('--width', '-w', type=int, default=100)
 args = parser.parse_args()
 
-import time
-
-args.rom[0].seek(args.offset)
-width = args.width
-im = Image.new('RGBA', (args.width,1024)) # create the Image of size 1 pixel 
+rom = args.rom[0]
+rom.seek(args.offset)
+im = Image.new('RGBA', (args.width, 1024))
 
 for y in range(0, 1024):
-    for x in range(0, width):
-        try:
-            data = args.rom[0].read(4)
-            im.putpixel( (x, y), (data[0], data[1], data[2], 255) ) 
-        except:
-            im.putpixel( (x, y), (0,0,0, 255) ) 
-            
+  for x in range(0, args.width):
+    try:
+      data = rom.read(4)
+      im.putpixel( (x, y), (data[0], data[1], data[2], 255) ) 
+    except:
+      im.putpixel( (x, y), (0,0,0, 255) ) 
+
 im.show()
--- xor_extractor.py
+++ xor_extractor.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
 
 import argparse
-import sys
 
-parser = argparse.ArgumentParser(description='Extract XOR-key from flash dump & RAM dump.')
+# Get arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('flash', nargs=1, type=argparse.FileType('rb'))
+parser.add_argument('flash',    nargs=1, type=argparse.FileType('rb'))
 parser.add_argument('ram_dump', nargs=1, type=argparse.FileType('rb'))
-parser.add_argument('xor_key', nargs=1, type=argparse.FileType('wb'))
+parser.add_argument('xor_key',  nargs=1, type=argparse.FileType('wb'))
 parser.add_argument('--length', '-l', type=int, default=40*1024)
 args = parser.parse_args()
 
+args.flash[0].seek(0x1E60)  # ROM file starts here for some reason
 
-LEN=args.length
-ram_dump = args.ram_dump[0].read(LEN)
-# Start of ROM in flash
-args.flash[0].seek(0x1e60)
-flash = args.flash[0].read(LEN)
+flash    = args.flash   [0].read(args.length)
+ram_dump = args.ram_dump[0].read(args.length)
+xor_key  = args.xor_key [0]
 
-def xor(b1, b2):
-    result = bytearray()
-    for b1, b2 in zip(b1, b2):
-        result.append(b1 ^ b2)
-    return result
+def xor(s1, s2):
+  """XOR 2 byte streams together"""
+  assert(len(s1) == len(s2))
+  return bytearray([
+    b1 ^ b2
+    for b1, b2 in zip(s1, s2)
+  ])
 
-args.xor_key[0].write(xor(ram_dump, flash))
-
-print(f"XOR stream successfully written!")
-
-
-
+xor_key.write(xor(ram_dump, flash))
